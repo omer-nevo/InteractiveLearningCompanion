00ecd0f6d77547ffa44b9330f8fe99b4fc6e7805
omer-nevo <81370933+omer-nevo@users.noreply.github.com>
2024-04-22 14:15:17 +0300

    Update 3_generate.py

--- pages/3_generate.py
+++ pages/3_generate.py
@@ -13,7 +13,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from google.generativeai.types import StopCandidateException
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.chains import ConversationChain
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import uuid
